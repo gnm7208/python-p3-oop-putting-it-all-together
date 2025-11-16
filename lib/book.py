8162d793e24a6bf94918ac9d22dcc6db04c48b81
George <gnm7208@gmail.com>
2025-11-16 21:44:18 +0300

    Refactor Book and Shoe classes for improved property handling and type validation

--- lib/book.py
+++ lib/book.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self,title,page_count):
+    def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
 
+    @property
     def page_count(self):
-        if not isinstance(self.page_count, int):
-            print("page count must be an integer")
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
         else:
-            self.page_count = self.page_count
-    
+            print("page_count must be an integer")
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-    
-    pass
-    
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self,brand,size):
+    def __init__(self, brand, size):
         self.brand = brand
         self.size = size
-        self.condition = "Used"
-    
-    def requires_int_size(self):
-        if not isinstance(self.size, int):
-            print("size must be an integer")
-        else:
-            self.size = self.size
-    
-    def cobble(self):
-        if self.condition != "New":
-            self.condition = "New"
-            print("Your shoe is as good as new!")
-        else:
-            print("Your shoe is already new!")
 
-    def __str__(self):
-        return f"This is a {self.brand} shoe of size {self.size} and condition {self.condition}"
-    pass
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
