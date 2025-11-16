2b99b5c5eee26ebf2629de545189825cf13b52ab
George <gnm7208@gmail.com>
2025-11-16 21:17:16 +0300

    Implement Book class with initialization and page handling methods

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self,title,page_count):
+        self.title = None
+        self.page_count = None
+
+    def page_count(self):
+        try:
+            self.page_count = int(self.page_count)
+        except ValueError:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
     pass
     
         
